fb752c5ccb9cbcd644874abcd1986e1c2d1c17f0
Ildar Aflitunov <igaflitun1@mts.ru>
2024-02-24 19:56:21 +0300

    Upd

--- Service/cmd/gometr/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "gotemplate/Service/internal/gometr/app"
-
-func main() {
-	app.Start()
-}
--- Service/internal/gometr/app/run.go
+++
@@ -1,6 +0,0 @@
-package app
-
-func (a *App) Run() {
-	a.log.Info("gometr is starting handle")
-	a.handler.StartHandle(a.cfg.HTTPHost, a.cfg.HTTPPort)
-}
